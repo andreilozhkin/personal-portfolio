a68a6e372b6e3722b39befe32b3d8ad956519a8b
Andrei Lozhkin <andreilozhkindev@gmail.com>
2019-05-30 16:25:58 +0700

    First commit

---
+++ portfolio/settings/dev.py
@@ -0,0 +1,7 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'c7vmu!8rrdz%ep5)bkj1$_3@(6&st7n$o3*at9i1so@cdese_w'
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
---
+++ portfolio/settings/prod.py
@@ -0,0 +1,8 @@
+from .base import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'mi-h^v%-=undu^yk0(rr$=2#coz$jok*^whpx-r0dn&y%apa5+'
+
+DEBUG = False
+ALLOWED_HOSTS = []
