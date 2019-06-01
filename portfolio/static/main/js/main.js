826c8fc1b826a19a655a0f4a90655bedee0abe70
Andrei Lozhkin <andreilozhkindev@gmail.com>
2019-06-02 02:21:05 +0700

    Update home page template, styles
    Added:
    	- sticky header
    	- mobile menu hides when click on menu item
    	- smooth anchor scroll
    	- project cards
    	- footer

---
+++ portfolio/static/main/js/main.js
@@ -0,0 +1,42 @@
+
+
+/* Toggle between showing and hiding the navigation menu */
+
+function toggleMenu() {
+  let nav = document.getElementsByClassName("header__nav")[0];
+
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  };
+};
+
+
+/* Collapse navigation menu on link click */
+
+function collapseMenu() {
+  if (!x.matches) {
+    console.log('smth');
+    let nav = document.getElementsByClassName("header__nav")[0];
+    nav.style.display = "none";
+  }
+};
+
+
+/* Auto expand/collapse */
+
+function expandMenu() {
+  let nav = document.getElementsByClassName("header__nav")[0];
+
+  if (x.matches) {
+    console.log('expand');
+    nav.style.display = "block";
+  } else {
+    console.log('collapse');
+    nav.style.display = "none";
+  };
+}
+
+var x = window.matchMedia("(min-width: 1000px)")
+x.addListener(expandMenu)
