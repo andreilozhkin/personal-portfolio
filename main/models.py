8e2c87c14b73b1176ef67b910364f5c90e181f88
Andrei Lozhkin <andreilozhkindev@gmail.com>
2019-06-03 03:38:27 +0700

    Update models.py
    1) Added URL field in Job, Education models.
    2) Changed description and logo image required in Job, Education models.
    3) Made end_date optional and display "Present" when null.
    4) Created Tag, Project models connected through ProjectTag(m2m).

--- main/models.py
+++ main/models.py
@@ -5,13 +5,14 @@ from django.db import models
 
 class Job(models.Model):
     company = models.CharField(max_length=30)
-    logo = models.ImageField(upload_to='companies', blank=True)
+    url = models.URLField()
+    logo = models.ImageField(upload_to='companies')
     country = models.CharField(max_length=2)
     city = models.CharField(max_length=24)
     role = models.CharField(max_length=30)
     start_date = models.IntegerField()
-    end_date = models.IntegerField()
-    description = models.TextField(blank=True)
+    end_date = models.IntegerField(blank=True, null=True)
+    description = models.TextField()
 
     class Meta:
         ordering = ['start_date']
@@ -21,7 +22,7 @@ class Job(models.Model):
         if self.start_date == self.end_date:
             return self.end_date
 
-        return '{} - {}'.format(self.start_date, self.end_date)
+        return '{} - {}'.format(self.start_date, self.end_date if self.end_date else 'Present')
 
     @property
     def location(self):
@@ -33,13 +34,14 @@ class Job(models.Model):
 
 class Education(models.Model):
     university = models.CharField(max_length=40)
-    logo = models.ImageField(upload_to='universities', blank=True)
+    url = models.URLField()
+    logo = models.ImageField(upload_to='universities')
     country = models.CharField(max_length=2)
     city = models.CharField(max_length=24)
     major = models.CharField(max_length=50)
     start_date = models.IntegerField()
-    end_date = models.IntegerField(blank=True)
-    description = models.TextField(blank=True)
+    end_date = models.IntegerField(blank=True, null=True)
+    description = models.TextField()
 
     class Meta:
         ordering = ['start_date']
@@ -49,7 +51,7 @@ class Education(models.Model):
         if self.start_date == self.end_date:
             return self.end_date
 
-        return '{} - {}'.format(self.start_date, self.end_date)
+        return '{} - {}'.format(self.start_date, self.end_date if self.end_date else 'Present')
 
     @property
     def location(self):
@@ -57,3 +59,34 @@ class Education(models.Model):
 
     def __str__(self):
         return '{}, {} {}'.format(self.major, self.university, self.date)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    projects = models.ManyToManyField('Project', through='ProjectTag', related_name='tags')
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='projects')
+    description = models.TextField()
+    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+
+    def __str__(self):
+        return self.name
+
+
+class ProjectTag(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} in {}'.format(self.tag, self.project)
