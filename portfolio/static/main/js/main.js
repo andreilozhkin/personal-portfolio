52ccf6c8024769e0825d8727a920066caab60892
Andrei Lozhkin <andreilozhkindev@gmail.com>
2019-06-02 18:05:29 +0700

    Header scroll down/hide, scroll up/show

--- portfolio/static/main/js/main.js
+++ portfolio/static/main/js/main.js
@@ -40,3 +40,45 @@ function expandMenu() {
 
 var x = window.matchMedia("(min-width: 1000px)")
 x.addListener(expandMenu)
+
+
+// Show header on scroll up
+
+function debounce(func, wait = 10, immediate = true) {
+      let timeout;
+      return function() {
+        let context = this, args = arguments;
+        let later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    };
+    let scrollPos = 0;
+
+    const nav = document.querySelector('.header');
+    function checkPosition() {
+
+      let windowY = window.scrollY;
+
+      if (windowY <= 200) {
+        nav.classList.add('is-visible');
+      }
+
+      else if (windowY < scrollPos) {
+        // Scrolling UP
+        nav.classList.add('is-visible');
+        nav.classList.remove('is-hidden');
+      } else {
+        // Scrolling DOWN
+        nav.classList.add('is-hidden');
+        nav.classList.remove('is-visible');
+      }
+      scrollPos = windowY;
+    }
+
+    window.addEventListener('scroll', debounce(checkPosition));
