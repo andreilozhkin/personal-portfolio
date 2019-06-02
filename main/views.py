c37bba7ae9850fe35e5fe00bd8548d52f52ce699
Andrei Lozhkin <andreilozhkindev@gmail.com>
2019-06-03 01:30:02 +0700

    Get Education objects in index view

--- main/views.py
+++ main/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from .models import Job
+from .models import Job, Education
 
 def index(request):
     jobs = Job.objects.all()
-    context = {'jobs': jobs}
+    education = Education.objects.all()
+    context = {
+        'jobs': jobs,
+        'education': education,
+    }
+    
     return render(request, 'main/index.html', context)
