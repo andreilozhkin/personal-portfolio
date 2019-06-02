0b5e598e983e21511623f26e602d12690d9cf42a
Andrei Lozhkin <andreilozhkindev@gmail.com>
2019-06-03 03:50:35 +0700

    Include projects in index view

--- main/views.py
+++ main/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from .models import Job, Education
+from .models import Job, Education, Project
 
 def index(request):
     jobs = Job.objects.all()
     education = Education.objects.all()
+    projects = Project.objects.all()
+
     context = {
         'jobs': jobs,
         'education': education,
+        'projects': projects,
     }
-    
+
     return render(request, 'main/index.html', context)
