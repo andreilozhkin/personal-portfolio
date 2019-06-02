456e5f85870529522cdf874dfacbc438d3a63fa9
Andrei Lozhkin <andreilozhkindev@gmail.com>
2019-06-03 03:48:24 +0700

    Add tag and project models to the admin site

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Job, Education
+from .models import Job, Education, Project, Tag
 
 # Register your models here.
 admin.site.register(Job)
 admin.site.register(Education)
+admin.site.register(Tag)
+
+class TagInline(admin.TabularInline):
+    model = Tag.projects.through
+    extra = 1
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = (
+        TagInline,
+    )
+
+admin.site.register(Project, ProjectAdmin)
