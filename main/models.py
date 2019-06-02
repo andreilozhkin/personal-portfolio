36a3e1dabcc5090c035eb59211d80bdfd3a81756
Andrei Lozhkin <andreilozhkindev@gmail.com>
2019-06-03 00:15:13 +0700

    Create Job model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Job(models.Model):
+    company = models.CharField(max_length=30)
+    logo = models.ImageField(upload_to='companies', blank=True)
+    country = models.CharField(max_length=2)
+    city = models.CharField(max_length=24)
+    role = models.CharField(max_length=30)
+    start_date = models.IntegerField()
+    end_date = models.IntegerField()
+    description = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ['start_date']
+
+    @property
+    def date(self):
+        if self.start_date == self.end_date:
+            return self.end_date
+
+        return '{} - {}'.format(self.start_date, self.end_date)
+
+    def __str__(self):
+        return '{}, {} {}'.format(self.role, self.company, self.date)
