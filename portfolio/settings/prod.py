463508d86b9841922273f7cf9aa9b5d4fdedeabf
Andrei Lozhkin <andreilozhkindev@gmail.com>
2019-06-06 01:55:30 +0700

    Remove production SECRET_KEY
    Please create a key.py with SECRET_KEY in settings folder to run app in
    production

--- portfolio/settings/prod.py
+++ portfolio/settings/prod.py
@@ -1,8 +1,6 @@
 from .base import *
+from  .key import SECRET_KEY
 
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mi-h^v%-=undu^yk0(rr$=2#coz$jok*^whpx-r0dn&y%apa5+'
 
 DEBUG = False
 ALLOWED_HOSTS = ['*']
